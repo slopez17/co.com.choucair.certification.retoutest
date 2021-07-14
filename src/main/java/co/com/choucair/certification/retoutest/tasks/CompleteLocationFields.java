254b06bb929bbf3e5ec52e9e3482f3c0b41eee35
Sandra Marcela Lopez Torres <slopezt@choucairtesting.com>
2021-07-14 18:21:24 -0500

    ajuste location

--- src/main/java/co/com/choucair/certification/retoutest/tasks/CompleteLocationFields.java
+++ src/main/java/co/com/choucair/certification/retoutest/tasks/CompleteLocationFields.java
@@ -1,6 +1,7 @@
 package co.com.choucair.certification.retoutest.tasks;
 
 import co.com.choucair.certification.retoutest.model.UtestData;
+import net.serenitybdd.core.time.InternalSystemClock;
 import net.serenitybdd.screenplay.Actor;
 import net.serenitybdd.screenplay.Task;
 import net.serenitybdd.screenplay.Tasks;
@@ -26,6 +27,7 @@ public class CompleteLocationFields implements Task {
 
     @Override
     public <T extends Actor> void performAs(T actor) {
+        new InternalSystemClock().pauseFor(500);
         actor.attemptsTo(
                 Enter.theValue(utestData.getStrCity()).into(INPUT_CITY),
                 Hit.the(ARROW_DOWN, ENTER).into(INPUT_CITY),
